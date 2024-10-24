30806d46398f667cf54a63146b57f2956a188b79
Sergey Kudashev <kudashevs@gmail.com>
2024-10-24 16:51:08 +0300

    Update tests add some string-inside-related tests

---
+++ test/removing-multi-line-inside.test.js
@@ -0,0 +1,30 @@
+const converter = require('./helpers/converter');
+const reader = require('./helpers/reader');
+const sut = require('../lib/index');
+
+describe('removing multi-line inline test suite', () => {
+  const input = reader('multi-line-inside');
+  const expected = `console.log('User was created ' + user.name + ' ' +  user.age);`;
+
+  it('can remove a multi-line comment inside string generated from a string parameter', () => {
+    const output = sut(input, {blocks: ['dev']});
+
+    expect(converter(output)).toBe(converter(expected));
+  });
+
+  it('can remove a multi-line comment inside string generated from an object parameter', () => {
+    let options = {
+      blocks: [
+        {
+          label: 'dev',
+          start: '/*',
+          end: '*/',
+        },
+      ],
+    };
+
+    const output = sut(input, options);
+
+    expect(converter(output)).toBe(converter(expected));
+  });
+});
