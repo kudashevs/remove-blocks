502d91932db3c6dbc7713ac4480d0dda75a1604b
Sergey Kudashev <kudashevs@gmail.com>
2024-10-24 13:54:58 +0300

    Refactor tests extract a multi-line test suite

---
+++ test/removing-multi-line-block.test.js
@@ -0,0 +1,31 @@
+const converter = require('./helpers/converter');
+const reader = require('./helpers/reader');
+const sut = require('../lib/index');
+
+describe('removing multi-line block test suite', () => {
+  const input = reader('multi-line-block');
+  const expected = `/* this comment should not be removed */
+module.exports = function addOne(num) {
+    const one = 1;
+    return num + one;
+}`;
+
+  it('can remove a multi-line comment from a string parameter', () => {
+    expect(sut(input, {blocks: ['debug']})).toBe(converter(expected));
+  });
+
+  it('can remove a multi-line comment from an object parameter and keepspace set to true', () => {
+    let options = {
+      blocks: [
+        {
+          label: 'debug',
+          start: '/*',
+          end: '*/',
+          keepspace: true,
+        },
+      ],
+    };
+
+    expect(sut(input, options)).toBe(converter(expected));
+  });
+});
