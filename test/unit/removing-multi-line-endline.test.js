df8db715df1c6e5c272465f5b53e474e9e0c14d3
Sergey Kudashev <kudashevs@gmail.com>
2024-11-01 16:25:20 +0300

    Update index.js add an end of a line case

---
+++ test/unit/removing-multi-line-endline.test.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const converter = require('../helpers/converter');
+const reader = require('../helpers/reader');
+const sut = require('../../lib/index');
+
+describe('remove multi-line inlined block from end of a line test suite', () => {
+  const input = reader('multi-line-endline');
+  const expected = `/* this comment should not be removed */
+module.exports = function addOne(num) {
+    const one = 1;
+    return num + one;
+}`;
+
+  it('can remove a multi-line comment generated from a string parameter', () => {
+    const output = sut(input, {blocks: ['debug']});
+
+    expect(converter(output)).toBe(converter(expected));
+  });
+
+  it('can remove a multi-line comment generated from an object parameter', () => {
+    const options = {
+      blocks: [
+        {
+          name: 'debug',
+          prefix: '/*',
+          suffix: '*/',
+        },
+      ],
+    };
+
+    const output = sut(input, options);
+
+    expect(converter(output)).toBe(converter(expected));
+  });
+});
